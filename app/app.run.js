fc258e633121a9ac76ab3b2beb08aeec59418a6e
José Cols <josecolsg@gmail.com>
2016-09-03 17:36:02 -0400

    Removed run config, while missing logic is done

--- app/app.run.js
+++ app/app.run.js
@@ -14,19 +14,19 @@
        * if the state does not requires authentication and the
        * user is logged in, redirect to the dashboard page.
        */
-      if (!toState.authenticate && Auth.isAuthenticated()) {
-        event.preventDefault();
-        $state.transitionTo('dashboard');
-      }
+      // if (!toState.authenticate && Auth.isAuthenticated()) {
+      //   event.preventDefault();
+      //   $state.transitionTo('dashboard');
+      // }
 
       /**
        * if the state requires authentication and the
        * user is not logged in, redirect to the login page.
        */
-      if (toState.authenticate && !Auth.isAuthenticated()) {
-        event.preventDefault();
-        $state.transitionTo('login');
-      }
+      // if (toState.authenticate && !Auth.isAuthenticated()) {
+      //   event.preventDefault();
+      //   $state.transitionTo('login');
+      // }
 
     });
   }
